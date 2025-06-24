7897019d2259ec421211d13b0c9f59ac6f6cce1d
abhigyan24379 <abhigyan03062003@gmail.com>
2025-06-24 15:03:09 +0530

    hello

--- Miscellaneous/1_Abalone.py
+++ Miscellaneous/1_Abalone.py
@@ -1,3 +1,24 @@
 import pandas as pd 
 abalone = pd.read_csv('abalone.data',header=None)
-print (abalone)
+# print (abalone)
+
+urlprefix = 'https://vincentarelbundock.github.io/Rdatasets/csv/'
+dataname = 'datasets/iris.csv'
+iris = pd.read_csv(urlprefix + dataname)
+
+# print (iris)
+
+# iris = iris.drop ('unnamed: 0',1)
+# print (iris)
+
+abalone.columns= ['Sex', 'Length', 'Diameter','Height','Whole weight','Shucked weight','Viscera weight','Shell weight', 'Rings']
+# print(abalone)
+
+# print(abalone['Sex'].describe())
+# print(pd.crosstab(abalone.Sex, abalone.Length ))
+# print(abalone['Height'].quantile(q=[0.25,0.5,0.75]))
+
+#Standard deviation 
+print(abalone['Height'].max() - abalone['Height'].min())
+
+print (round(abalone["Shell weight"].var(), 2)) # rounds to two decimal places 
