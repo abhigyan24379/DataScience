76932f9ae708cea908e663161015d71fa33a063e
abhigyan24379 <abhigyan03062003@gmail.com>
2025-06-29 18:52:01 +0530

    Bayes Theorem Application

---
+++ Week3/3_Probability/2_ex.py
@@ -0,0 +1,18 @@
+# Problem 
+# - A disease affects 1% of the population 
+# - A test is 95% accurate for disease individuals and 90% accurate for non diseased individual 
+# - Find the probability of having the disease given a positive test result 
+
+def bayes_theorem ( prior, sensitivity , specificity ):
+    evidence = ( sensitivity * prior ) + ((1-specificity) * (1 - prior))
+    posterior = ( sensitivity * prior ) /evidence
+    return posterior
+
+prior = 0.01
+sensitivity  = 0.95
+specificity = 0.90
+
+posterior = bayes_theorem(prior, sensitivity, specificity)
+print(posterior)
+
+
